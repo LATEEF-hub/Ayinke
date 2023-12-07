73ed328c95f947c6018c5b6ba18bc2e21f6a114b
LATEEF-hub <latifkk@krok.edu.ua>
2023-12-07 09:44:09 +0000

    create function remove cart

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,32 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
     cart.classList.remove('active');
 }
+
+
+if(document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready)
+}else{
+    ready();
+}
+
+// making function
+
+function ready(){
+    //remove item from cart
+    var removeCartButton = document.getElementsByClassName('cart-remove');
+    console.log(removeCartButton)
+    for (var i = 0; i < removeCartButton.length; i++) {
+        const buttons = removeCartButton[i];
+        buttons.addEventListener('click', removeCartItem);
+        
+    }
+}
+
+function removeCartItem(event) {
+    /**
+     * function to remove product from Cart
+     */
+    var buttonClicked = event.target
+    buttonClicked.parentElement.remove();
+
+}
